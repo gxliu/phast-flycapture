3bd1e70e3e62e6c114da1a8bfcd84e8c92d27409
Anshula Gandhi <anshula.gandhi@gmail.com>
2014-08-04 17:58:09 -0400

    clean

--- ShutterSpeedsUSBGigE/ShutterSpeeds.cpp
+++ ShutterSpeedsUSBGigE/ShutterSpeeds.cpp
@@ -1,22 +1,3 @@
-//=============================================================================
-// Copyright � 2010 Point Grey Research, Inc. All Rights Reserved.
-//
-// This software is the confidential and proprietary information of Point
-// Grey Research, Inc. ("Confidential Information").  You shall not
-// disclose such Confidential Information and shall use it only in
-// accordance with the terms of the license agreement you entered into
-// with PGR.
-//
-// PGR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
-// SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-// PURPOSE, OR NON-INFRINGEMENT. PGR SHALL NOT BE LIABLE FOR ANY DAMAGES
-// SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
-// THIS SOFTWARE OR ITS DERIVATIVES.
-//=============================================================================
-//=============================================================================
-// $Id: GigEGrabEx.cpp,v 1.10 2010-06-01 18:19:44 soowei Exp $
-//=============================================================================
 
 
 /* 
